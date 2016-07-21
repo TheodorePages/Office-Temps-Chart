803883e713768e32622a114d6deb6aaa5d8b4e92
TheodorePages <tiej1990@gmail.com>
2016-07-21 16:03:35 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,108 +1,151 @@
-google.charts.load("current", {packages:["corechart"]});
-google.charts.setOnLoadCallback(drawChart);
-var data;
-var dataStrings=[];
 var chart;
+var data;
 var options;
-var lastValue=0;
-var currentVal=0;
-var lastUpdated;
-var maxValue=0;
-var timer;
-var lastTimer=0;
-var button;
-
-function setup()
-{
-button=createButton('download');
-button.position(windowWidth/2,windowHeight*7/10);
-button.mousePressed(downloadText);
-}
-
+var chartReady=false;
+var downloadButton;
+var catchupButton;
+var loadTimer=0;
+google.charts.load("current", {packages:["corechart"]}); 
+google.charts.setOnLoadCallback(chartInitialization);
+var singleTempUpdateAddress="http://data.theodoretech.com/OfficeTempCurrent/?key=8385";
+var todayTempUpdateAddress="http://data.theodoretech.com/OfficeTempsToday/?key=8385";
 function windowResized()
 {
-options.width=windowWidth;
-options.height=windowHeight/2;
-button.position=windowWidth/2,windowHeight*7/10;
+
+buttonLocs();
 }
 
-function downloadText()
-{var filename="Session-"+hour()+"_"+minute()+"_"+second()+".txt";
-
-saveStrings(dataStrings,filename);
+function setup() {
+downloadButton=createButton('download');
+downloadButton.mousePressed(downloadButtonCB);
+catchupButton = createButton("GetToday's Temps");
+catchupButton.mousePressed(catchupButtonCB);
+buttonLocs();
 }
 
-function draw()
-{
-if((millis()-lastTimer)<3000||lastTimer==0)
-  {//updateData();
-  lastTimer=millis();
-  var datajson;
-  datapass(datajson);
-  }  
-}
-
-function updateData()
-{
-var address="http://data.theodoretech.com/OfficeTemp.json/?key=8385";
-loadJSON(address,datapass);
-println(address);
-}
-
-function datapass(newData)
-{
-//if(!isNaN(newData.OfficeTemp))
-  //{var newVal=newData.OfficeTemp;
- // currentVal=newData.OfficeTemp;
-  currentVal=random(70,80)//temporary commit random numbers while server is down for testing
-  lastUpdated=hour()+":"+minute()+":"+second();
-  println(currentVal);
-  data.addRow([[hour(),minute(),second()],currentVal]);
-  chart.draw(data, options);
-  var textDat = document.getElementById("textData");
-  textDat.innerHTML +="<br>"+hour()+":"+minute()+":"+second()+", "+currentVal;
-  dataStrings=append(dataStrings,hour()+":"+minute()+":"+second()+", "+currentVal);
-  if(currentVal>maxValue)
-    {maxValue=currentVal;
-    var maxDat = document.getElementById("maxData");
-    maxDat.innerHTML =" Max Temperature is: "+currentVal;
-    }
-  //}
-}
-
-
-function drawChart() {
-      data = new google.visualization.DataTable();
-      data.addColumn('timeofday', 'Time of Day');
-      data.addColumn('number','Temp');
-
-      // Our central point, which will jiggle.
-      //data.addRow([0, 0]);
-      
-      options = {
+function buttonLocs()
+{if(windowWidth<displayWidth/2)
+  {
+  catchupButton.position(windowWidth/2,windowHeight*1/20);
+  downloadButton.position(windowWidth/2,windowHeight*1.75/20);
+  }
+else{
+  catchupButton.position(windowWidth*1/3,windowHeight*1/20);
+  downloadButton.position(windowWidth/2,windowHeight*1/20);
+  }
+if(chartReady){
+  options = {
         legend: 'none',
-        'width':windowWidth,
+        'width':windowWidth*.9,
         'height':windowHeight/2,
         'hAxis': { 'title': 'Time'},
         'vAxis': { 'title': 'Temperature',
                 viewWindow: {
                     min: 0,
-                    max: 250
+                    max: 150
                     }
-        },
-        animation: {
-          duration: 200,
-          easing: 'inAndOut',
-        }
+        }//,
+        //animation: {
+          //duration: 10,
+          //easing: 'inAndOut',
+        //}
       };
+  }
+}
 
-      chart = new google.visualization.ScatterChart(document.getElementById('animatedshapes_div'));
+function draw() {
 
-      // Start the animation by listening to the first 'ready' event.
-      google.visualization.events.addOneTimeListener(chart, 'ready', datapass);
-
-      // Control all other animations by listening to the 'animationfinish' event.
-      google.visualization.events.addListener(chart, 'animationfinish', datapass);
-
-      chart.draw(data, options);
+if(chartReady)
+  {  
+  if(millis()-loadTimer>10000||loadTimer==0)
+    {loadStrings(singleTempUpdateAddress,liveDataUpdate) 
+    loadTimer=millis();
     }
+  }
+}
+
+
+function liveDataUpdate(data)
+{println(data);
+var dataSplit=splitTokens(String(data),",");
+println(dataSplit[0]);
+var timeOfTemp=splitTokens(dataSplit[1],":");
+var temp=float(dataSplit[2]);
+addDataPoint(timeOfTemp,float(dataSplit[2]),false);
+
+}
+
+function catchupButtonCB()
+{
+loadStrings(todayTempUpdateAddress,catchupToday) 
+}
+
+function catchupToday(data)
+{
+println(data);
+
+var dataSplit=splitTokens(String(data),"_");
+for(var i=0; i<dataSplit.length-1; i++)
+  {
+  var dataSplit2=splitTokens(dataSplit[i],",");
+  var timeOfTemp=splitTokens(dataSplit2[1],":");
+  var temp=float(dataSplit2[2]);
+  addDataPoint(timeOfTemp,float(dataSplit2[2]),true);
+  }
+var dataSplit2=splitTokens(dataSplit[dataSplit.length-1],",");
+  var timeOfTemp=splitTokens(dataSplit2[1],":");
+  var temp=float(dataSplit2[2]);
+  addDataPoint(timeOfTemp,float(dataSplit2[2]),false);
+}
+
+function addDataPoint(timeVal,value,holdDraw)
+{//println(value);
+data.addRow([[int(timeVal[0]),int(timeVal[1]),int(timeVal[2])],value]);
+//wait to draw if more data is coming
+if(holdDraw==false)
+  {
+  chart.draw(data,options);
+  }
+}
+
+function downloadButtonCB()
+{
+var filename="Session-"+day()+"_"+month()+"_"+year()+"-"+hour()+"_"+minute()+"_"+second()+".txt";
+saveStrings(data,filename); 
+}
+
+function chartInitialization()
+{data = new google.visualization.DataTable();
+chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+data.addColumn('timeofday', 'Time of Day');
+//data.addColumn('number','Temp');
+data.addColumn('number','Temp');
+//data.addRow([2,4]);
+//data.addRow([[int(hour()),int(minute()),int(second())],float(2)]);
+options = {
+        legend: 'none',
+        'width':windowWidth*.9,
+        'height':windowHeight/2,
+        'hAxis': { 'title': 'Time'},
+        'vAxis': { 'title': 'Temperature',
+                viewWindow: {
+                    min: 0,
+                    max: 150
+                    }
+        }//,
+        //animation: {
+          //duration: 10,
+          //easing: 'inAndOut',
+        //}
+      };
+chart.draw(data,options);
+chartReady=true;
+println("chartReady");
+}
+
+
+
+
+
+
+
